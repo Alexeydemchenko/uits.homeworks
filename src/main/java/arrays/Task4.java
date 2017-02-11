146ae11703074c6a9427c22a877bc0cf195a6312
alex-d <alexeydemchenko@protonmail.com>
2017-02-11 21:52:36 +0200

    Bubble sort Task4

---
+++ src/main/java/arrays/Task4.java
@@ -0,0 +1,34 @@
+package arrays;
+import java.util.Random;
+
+public class Task4 {
+    public static void main(String[] args){
+        Random rand = new Random();
+        int[] arr = new int[10];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = rand.nextInt(20);
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        
+        bubbleSort(arr);
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void bubbleSort(int[] arr){
+        int n = arr.length;
+        int temp = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 1; j < (n - i); j++){
+                if(arr[j - 1] > arr[j]){
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+    
+}

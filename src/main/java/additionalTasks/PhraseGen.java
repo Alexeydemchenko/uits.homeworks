0d12bd48f467843aee2dbf8b2b5898943b5cc67b
alex-d <alexeydemchenko@protonmail.com>
2017-03-04 20:53:22 +0200

    StringBuilder task added

--- src/main/java/additionalTasks/PhraseGen.java
+++ src/main/java/additionalTasks/PhraseGen.java
@@ -1,8 +1,38 @@
 package additionalTasks;
-import java.util.*;
+import java.lang.StringBuilder;
 public class PhraseGen {
     public static void main(String[] args){
-
+        String[] firstWord = {"Дорогие друзья!", " с другой стороны","равным "
+                               + " образом"," Таким образом, "};
+        String[] secondWord = {" рамки и место обучения кадров", " новая модель",
+                                " укрепления и развития структуры"};
+        String[] thirdWord = {" позволяет оценить значение ", " позволяет выполнить ",
+                              " требуют от нас анализа "};
+        String[] fourthWord = {" системы массового участия", " новых предложений",
+                              " соответствующих условий активизации"};
+        
+        int firstLength = firstWord.length;
+        int secondLength = secondWord.length;
+        int thirdLength = thirdWord.length;
+        int fourthLength = fourthWord.length;
+        
+        int rand1 = (int) (Math.random() * firstLength);
+        int rand2 = (int) (Math.random() * secondLength);
+        int rand3 = (int) (Math.random() * thirdLength);
+        int rand4 = (int) (Math.random() * fourthLength);
+        
+        StringBuilder str = new StringBuilder();
+        str.append(firstWord[rand1]);
+        str.append(" ");
+        str.append(secondWord[rand2]);
+        str.append(" ");
+        str.append(thirdWord[rand3]);
+        str.append(" ");
+        str.append(fourthWord[rand4]);
+        str.append(" ");
+        
+        String phrase = str.toString();
+        System.out.println(phrase);
     }
 }
 

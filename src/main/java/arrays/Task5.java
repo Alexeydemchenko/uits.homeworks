e6a6e07a666aa65f6efec9f7c24ff36b3d41b1a6
alex-d <alexeydemchenko@protonmail.com>
2017-03-16 20:56:29 +0200

    Task 5. End

--- src/main/java/arrays/Task5.java
+++ src/main/java/arrays/Task5.java
@@ -8,8 +8,22 @@ public class Task5 {
         System.out.println("Введите количество столбцов");
         int col = scan.nextInt();
         int[][] mat = createNewMatrix(ln, col);
+        System.out.println("\n---Начальная матрица (1 вариант)---\n");
         viewMatrix(mat);
-        
+        System.out.println("\n---Начальная матрица (2 вариант)---\n");
+        viewMatrix2(mat);
+        System.out.println("\n---Перестановка строк и элементов---\n");
+        reverseMatrix(mat);
+        System.out.println("\n---Перестановка элементов---\n");
+        reverseMatrix2(mat);
+        System.out.println("\n---Четные элементы---\n");
+        evenMatrixElement(mat);
+        System.out.println("\n---Нечетные элементы---\n");
+        oddMatrixElement(mat);
+        System.out.println("\n---Кратные семи элементы---\n");
+        multipleSeven(mat);
+        System.out.println("\n---Произведение трех---\n");
+        multipleThree(mat);
     }
     
     //метод для создания матрицы
@@ -24,14 +38,94 @@ public class Task5 {
         return mat;
     }
     
-    // метод для выведения всех элементов матрицы
-    static void viewMatrix(int[][] array){
-        for(int[] lines : array){
+    // метод для выведения всех элементов матрицы (1 вариант)
+    static void viewMatrix(int[][] arr){
+        for(int[] lines : arr){
             for(int element : lines){
                 System.out.print(element + " ");
             }
             System.out.println();
         }
+    }
+    
+    // метод для выведения всех элементов матрицы (2 вариант)
+    static void viewMatrix2(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    // метод для выведения матрицы в обратную сторону
+    static void reverseMatrix(int[][] arr){
+        for(int i = arr.length-1; i >= 0; i--){
+            for(int j = arr[i].length - 1; j >=0; j--){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    // метод для выведения матрицы в обратную сторону
+    static void reverseMatrix2(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = arr[i].length - 1; j >= 0; j--){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    // метод для четных элемнтов матрицы
+    static void evenMatrixElement(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if((i % 2 == 0) && (arr[i][j] % 2 == 0)){
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    // метод для нечетных элемнтов матрицы
+    static void oddMatrixElement(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if(i % 2 != 0 && arr[i][j] % 2 != 0){
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    // метод для кратных семи
+    static void multipleSeven(int[][] arr){
+        int multSevSum = 0;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if((i % 2 == 0) && (arr[i][j] % 7 == 0) && (arr[i][j] > 0)){
+                    multSevSum += arr[i][j];
+                }
+            }
+        }
+        System.out.println("summa " + multSevSum);
+    }
+    
+    // метод для трех
+    static void multipleThree(int[][] arr){
+        int multThreeSum = 0;
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                if((i % 2 != 0) && (arr[i][j] % 3 == 0) && (arr[i][j] > 0)){
+                    multThreeSum += arr[i][j];
+                }
+            }
+        }
+        System.out.println("summa " + multThreeSum);
     }
     
 }
